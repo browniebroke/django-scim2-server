0ecc54df694fd11d1925b9708c7a7353cb2faa11
Bruno Alla <alla.brunoo@gmail.com>
2026-03-21 18:13:10 +0000

    fix: make user OneToOneField work with custom user models

--- src/django_scim2_server/models.py
+++ src/django_scim2_server/models.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 import uuid
 
+from django.conf import settings
 from django.db import models
 
 
@@ -12,7 +13,7 @@ class SCIMUser(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(
-        "auth.User",
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="scim",
     )
